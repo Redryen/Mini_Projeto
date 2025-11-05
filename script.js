b0c8df503016ce1827c9a29ef1b6bd951f567768
LorennaJV <>
2025-11-05 13:30:31 -0300

    save

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-var cores = ["#6cbaff", "#6cff8f", "#c86cff"]; 
-var indiceCor = 0;
+var cores = ["#6cbaff", "#6cff8f", "#c86cff"]
+var indiceCor = 0
 
 function adicionarNota() {
-  var texto = document.getElementById("textoNota").value;
-  var divContainer = document.getElementById("container");
-  var urgenciaSelecionada = document.querySelector('input[name="urgencia"]:checked').value;
+  var texto = document.getElementById("textoNota").value
+  var divContainer = document.getElementById("container")
+  var urgenciaSelecionada = document.querySelector('input[name="urgencia"]:checked').value
 
-  var novaNota = document.createElement("div");
-  novaNota.classList.add("nota");
+  var novaNota = document.createElement("div")
+  novaNota.classList.add("nota")
 
 //Sequencia da cor do fundo
-  novaNota.style.backgroundColor = cores[indiceCor];
-  indiceCor = (indiceCor + 1) % cores.length;
+  novaNota.style.backgroundColor = cores[indiceCor]
+  indiceCor = (indiceCor + 1) % cores.length
 
   if (urgenciaSelecionada === "sim") {
-    novaNota.classList.add("urgente");
+    novaNota.classList.add("urgente")
   } else {
-    novaNota.classList.add("nao-urgente");
+    novaNota.classList.add("nao-urgente")
   }
 
-  var textoNota = document.createElement("span");
-  textoNota.innerText = texto;
+  var textoNota = document.createElement("span")
+  textoNota.innerText = texto
 
-  novaNota.appendChild(textoNota);
-  divContainer.appendChild(novaNota);
+  novaNota.appendChild(textoNota)
+  divContainer.appendChild(novaNota)
 
-  document.getElementById("textoNota").value = "";
+  document.getElementById("textoNota").value = ""
 }
 
 function apagarTudo() {
-  var container = document.getElementById("container");
+  var container = document.getElementById("container")
   container.innerHTML = "";
 }
 
 function removerUltimaNota() {
-  var container = document.getElementById("container");
-  var ultNota = container.lastElementChild;
+  var container = document.getElementById("container")
+  var ultNota = container.lastElementChild
   if (ultNota) {
-    container.removeChild(ultNota);
+    container.removeChild(ultNota)
   }
 }
